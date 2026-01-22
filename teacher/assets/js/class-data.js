bc3f163e55fe4ac3540c488ca0b7d45c8a4adb85
nolopez338 <37830271+nolopez338@users.noreply.github.com>
2026-01-22 10:52:45 -0500

    Refactor class pages to dynamic renderer

---
+++ teacher/assets/js/class-data.js
@@ -0,0 +1,65 @@
+window.CLASS_STUDENT_LISTS = {
+  "10A": {
+    pdfUrl:
+      "https://correosanbartoloedu-my.sharepoint.com/:b:/g/personal/nicolas_lopez_sanbartolo_edu_co/IQDe06pqnnnnSZs23p-u1ZubAXsK66kBIR5AM-Oi4lmrg9A?e=TyX9ap",
+    students: [],
+  },
+  "10B": {
+    pdfUrl:
+      "https://correosanbartoloedu-my.sharepoint.com/:b:/g/personal/nicolas_lopez_sanbartolo_edu_co/IQAx9bJVKfMuSIp0MDpo-kZ7ASHF83XVSgRhf-Hz3LWzc3g?e=rJ1qxB",
+    students: [],
+  },
+  "10C": {
+    pdfUrl:
+      "https://correosanbartoloedu-my.sharepoint.com/:b:/g/personal/nicolas_lopez_sanbartolo_edu_co/IQBlQ8VmL1LpSJXvVV_n0NeTAe5O4N0POxK7cEuWR2rjCi8?e=H4zzl4",
+    students: [],
+  },
+  "10D": {
+    pdfUrl:
+      "https://correosanbartoloedu-my.sharepoint.com/:b:/g/personal/nicolas_lopez_sanbartolo_edu_co/IQAGdXuhcZKRQ6kF0TaDfI45AaD-gSxBCtc7cCt1p1u72-s?e=fkC2er",
+    students: [],
+  },
+  "10E": {
+    pdfUrl:
+      "https://correosanbartoloedu-my.sharepoint.com/:b:/g/personal/nicolas_lopez_sanbartolo_edu_co/IQBHimNLEtR4T76nmWm5R4BoASlxoR_9yOKsGMqyx3gTYos?e=XdF90L",
+    students: [],
+  },
+  "11A": {
+    pdfUrl:
+      "https://correosanbartoloedu-my.sharepoint.com/:b:/g/personal/nicolas_lopez_sanbartolo_edu_co/IQDzrNnkmXbQSJpTDFpxuODuAWKzdJBu0sBfgErbIT4aakA?e=Jr0TOM",
+    students: [],
+  },
+  "11B": {
+    pdfUrl:
+      "https://correosanbartoloedu-my.sharepoint.com/:b:/g/personal/nicolas_lopez_sanbartolo_edu_co/IQACkgMVyoQoT7ua0BJqbXfbATYjinUCodtCGg6Qi7AEve0?e=KGtJMm",
+    students: [],
+  },
+  "11C": {
+    pdfUrl:
+      "https://correosanbartoloedu-my.sharepoint.com/:b:/g/personal/nicolas_lopez_sanbartolo_edu_co/IQCPTFnw99QMTKpR0e_L78oOAWKtS7WXVetw1PED2hlPfrk?e=24RuBc",
+    students: [],
+  },
+  "11D": {
+    pdfUrl:
+      "https://correosanbartoloedu-my.sharepoint.com/:b:/g/personal/nicolas_lopez_sanbartolo_edu_co/IQAh4xNXLXKZSLMsT11KqHThAW_pW8TOPgi-Ym3_P5Pf23k?e=6OdS0H",
+    students: [],
+  },
+  "11E": {
+    pdfUrl:
+      "https://correosanbartoloedu-my.sharepoint.com/:b:/g/personal/nicolas_lopez_sanbartolo_edu_co/IQBB5aqr_WIBTLwvfP0MiQYPAeDWo8n9EbDxE7bKUJ0zyYU?e=PpcPU5",
+    students: [],
+  },
+};
+
+window.CLASS_NOTES = {
+  "10A": [],
+  "10B": [],
+  "10C": [],
+  "10D": [],
+  "10E": [],
+  "11A": [],
+  "11B": [],
+  "11C": [],
+  "11D": [],
+  "11E": [],
+};
