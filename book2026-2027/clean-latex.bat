eff8e2743014fbc1b503f42266a7b3565b472cac
Nicolas Lopez <nicolas.lopez.lc.994@gmail.com>
2026-08-04 11:09:57 -0500

    Update latex clean bat

--- book2026-2027/clean-latex.bat
+++ book2026-2027/clean-latex.bat
@@ -3,9 +3,9 @@ echo Deleting LaTeX auxiliary files recursively...
 
 del /s /q *.aux 2>nul
 del /s /q *.synctex.gz 2>nul
-del /s /q *.log 2>nul
+
 del /s /q *.out 2>nul
 del /s /q *.toc 2>nul
-
+del /s /q *.synctex 2>nul
 echo Done.
 pause
