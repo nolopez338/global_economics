1239949bff6cd9a20539b3fff280cfdc37057179
Nicolas Lopez <nicolas.lopez.lc.994@gmail.com>
2026-08-11 16:03:04 -0500

    Update calendar data

--- teacher/assets/js/year-calendar-data.js
+++ teacher/assets/js/year-calendar-data.js
@@ -229,9 +229,9 @@ const calendar2026 = {
 
 const holidays2026 = [
   { date: "2026-08-07", name: "Batalla de Boyacá" },
-  { date: "2026-08-15", name: "Asunción de la Virgen" },
-  { date: "2026-10-19", name: "Día de la Raza" },
-  { date: "2026-11-01", name: "Todos los Santos" },
+  { date: "2026-08-17", name: "Asunción de la Virgen" },
+  { date: "2026-10-12", name: "Día de la Raza" },
+  { date: "2026-11-02", name: "Todos los Santos" },
   { date: "2026-11-16", name: "Independencia de Cartagena" },
   { date: "2026-12-08", name: "Inmaculada Concepción" },
   { date: "2026-12-25", name: "Navidad" }
